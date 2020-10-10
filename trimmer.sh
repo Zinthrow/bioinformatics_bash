67d6ba06c1ec52737b01c69d6b7a102202dd0583
Alex Larsen <33006657+Zinthrow@users.noreply.github.com>
2020-10-09 20:59:37 -0400

    Update trimmer.sh

--- trimmer.sh
+++ trimmer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git clone https://github.com/lh3/seqtk.git;
+cd seqtk; make
+
 val=_R
 for file in *$val*; do mv $file ${file:0:20}.full.fastq.gz; done
 for file in *$val*; do seqtk sample -s100 $file 12000000 > ${file:0:-13}fastq;done
